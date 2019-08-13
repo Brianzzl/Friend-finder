a4f944e714b6c93055b1067425a5ffbe39d86299
Zheli Zhu <brianzzl@hotmail.com>
2019-08-12 22:56:32 -0400

    added the html and friend.js

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [
+    {
+        'name':"Tyrion Lannister",
+        'photo':"https://vignette.wikia.nocookie.net/gameofthrones/images/9/95/HandoftheKingTyrionLannister.PNG/revision/latest?cb=20190520175204",
+        'score':[2,5,4,4,3,1,2,5,3,4]
+    },
+    {
+        'name':"John Snow",
+        'photo':"https://vignette.wikia.nocookie.net/gameofthrones/images/d/d0/JonSnow8x06.PNG/revision/latest?cb=20190714094440",
+        'score':[3,5,3,1,3,2,2,4,4,5]
+    },
+    {
+        'name':"Arya Stark",
+        'photo':"https://vignette.wikia.nocookie.net/gameofthrones/images/b/be/AryaShipIronThrone.PNG/revision/latest/scale-to-width-down/335?cb=20190520174300",
+        'score':[1,2,4,3,2,5,3,2,5,3]
+    },
+    {
+        'name':"Daenerys Targaryen",
+        'photo':"https://vignette.wikia.nocookie.net/gameofthrones/images/e/ee/QueenDaenerysTargaryenIronThrone.PNG/revision/latest?cb=20190520173137",
+        'score':[3,5,2,1,1,2,2,5,4,3]
+    }
+]
+
+module.exports =  friends;
