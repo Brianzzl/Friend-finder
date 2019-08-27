07b2bc6aa31b2aa0238e3ea6a49d1557dd5eefad
Zheli Zhu <brianzzl@hotmail.com>
2019-08-26 23:52:16 -0400

    fix bug for picture not showing properly

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friends = [
     {
         'name':"Tyrion Lannister",
-        'photo':"https://vignette.wikia.nocookie.net/gameofthrones/images/9/95/HandoftheKingTyrionLannister.PNG/revision/latest?cb=20190520175204",
+        'photo':"https://media2.giphy.com/media/iwVHUKnyvZKEg/200.webp?cid=790b7611bd3330d7d91ac33298d76f801e12bc642385d3ee&rid=200.webp",
         'score':[2,5,4,4,3,1,2,5,3,4]
     },
     {
         'name':"John Snow",
-        'photo':"https://vignette.wikia.nocookie.net/gameofthrones/images/d/d0/JonSnow8x06.PNG/revision/latest?cb=20190714094440",
+        'photo':"https://vignette.wikia.nocookie.net/gameofthrones/images/d/d0/JonSnow8x06.PNG/",
         'score':[3,5,3,1,3,2,2,4,4,5]
     },
     {
         'name':"Arya Stark",
-        'photo':"https://vignette.wikia.nocookie.net/gameofthrones/images/b/be/AryaShipIronThrone.PNG/revision/latest/scale-to-width-down/335?cb=20190520174300",
+        'photo':"https://vignette.wikia.nocookie.net/gameofthrones/images/b/be/AryaShipIronThrone.PNG/",
         'score':[1,2,4,3,2,5,3,2,5,3]
     },
     {
         'name':"Daenerys Targaryen",
-        'photo':"https://vignette.wikia.nocookie.net/gameofthrones/images/e/ee/QueenDaenerysTargaryenIronThrone.PNG/revision/latest?cb=20190520173137",
+        'photo':"https://vignette.wikia.nocookie.net/gameofthrones/images/e/ee/QueenDaenerysTargaryenIronThrone.PNG/",
         'score':[3,5,2,1,1,2,2,5,4,3]
     }
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,19 +6,21 @@ module.exports=function(app){
 
 //get request from client
 app.get("/api/friends",function(req,res){
+
     res.json(friends);
 });
 
 
 
-// post request 
+// post request
 
 app.post("/api/friends", function(req, res) {
-  
-    
+
+
  friends.push(req.body);
+ 
         res.json(true);
-   
+
 
   });
 
